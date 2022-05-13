7956cad0c0f7f23ba2a435a6e9a46aee79bafced
Aleksandra Popova <alexnika@bk.ru>
2022-05-13 19:54:39 +0300

    added task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Найти расстояние между точками в пространстве 2D.
+
+
+Console.WriteLine("Давайте найдем расстояние между точками А и В в пространстве 2D");
+Console.WriteLine("Введите координату X для точки А: ");
+int AnumX = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Y для точки А: ");
+int AnumY = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координату X для точки B: ");
+int BnumX = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Y для точки B: ");
+int BnumY = int.Parse(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(BnumX - AnumX,2) + Math.Pow(BnumY - AnumY,2));
+Console.WriteLine($"Расстояние между точками А и В в пространстве 2D равно {result}");
+
+    
+
+
+
+
+
+
